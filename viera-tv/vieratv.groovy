533bbb05fdd54052652ef01267b1d0b6601af5e4
alex.baloc <alex.baloc@gmail.com>
2017-11-12 12:58:59 +0200

    WIP hdmi support

--- viera-tv/vieratv.groovy
+++ viera-tv/vieratv.groovy
@@ -19,62 +19,121 @@ metadata {
 	definition (name: "Panasonic VIERA St60", namespace: "alexbaloc", author: "alexbaloc") {
     capability "Switch"
 			command "mute" 
-			command "source"
-			command "menu"    
-      command "close"
+			command "tv"
+      command "hdmi1"
 	}
 
-    preferences {
+  preferences {
     input("TvIP", "string", title:"TV IP Address", description: "Please enter your TV's IP Address", required: true, displayDuringSetup: true)
+    input("Hdmi1Label", "string", title:"HDMI1 input name", description: "Enter a name for the HDMI input if you want to use it in an action", required: false, displayDuringSetup: true)
+    input("Hdmi1CustomIconName", "string", title:"HDMI1 input custom icon name", description: "Smartthings icon name for this input", required: false, displayDuringSetup: true)
+    input("Hdmi2Label", "string", title:"HDMI2 input name", description: "Enter a name for the HDMI input if you want to use it in an action", required: false, displayDuringSetup: true)
+    input("Hdmi3Label", "string", title:"HDMI3 input name", description: "Enter a name for the HDMI input if you want to use it in an action", required: false, displayDuringSetup: true)
 	}
     
 	simulator {
     
 	}
 
-    tiles {
-        standardTile("switch", "device.switch", width: 1, height: 1, canChangeIcon: true) {
-            state "default", label:'TV', action:"switch.off", icon:"st.Electronics.electronics15", backgroundColor:"#ffffff"
-        }
-        standardTile("power", "device.switch", width: 1, height: 1, canChangeIcon: false) {
-            state "default", label:'', action:"switch.off", decoration: "flat", icon:"st.thermostat.heating-cooling-off", backgroundColor:"#ffffff"
-        }
-        standardTile("mute", "device.switch", decoration: "flat", canChangeIcon: false) {
-            state "default", label:'Mute', action:"mute", icon:"st.custom.sonos.muted", backgroundColor:"#ffffff"
-        }    
-        standardTile("source", "device.switch", decoration: "flat", canChangeIcon: false) {
-            state "default", label:'Source', action:"source", icon:"st.Electronics.electronics15"
-        }
-       standardTile("menu", "device.switch", decoration: "flat", canChangeIcon: false) {
-            state "default", label:'Menu', action:"menu", icon:"st.vents.vent"
-        }        
-        main "switch"
-        details([ "power", "mute", "source", "menu"])
-    }
+  tiles {
+      standardTile("switch", "device.switch", width: 1, height: 1, canChangeIcon: true) {
+          state "default", label:'TV', action:"switch.off", icon:"st.Electronics.electronics15", backgroundColor:"#ffffff"
+      }
+      standardTile("power", "device.switch", width: 2, height: 2, canChangeIcon: false) {
+          state "default", label:'', action:"switch.off", decoration: "flat", icon:"st.thermostat.heating-cooling-off", backgroundColor:"#f45c42"
+      }
+      standardTile("mute", "device.switch", decoration: "flat", canChangeIcon: false) {
+          state "default", label:'Mute', action:"mute", icon:"st.custom.sonos.muted", backgroundColor:"#ffffff"
+      }    
+      standardTile("tv", "device.switch", decoration: "flat", canChangeIcon: false) {
+          state "default", label:'Input: TV', action:"tv", icon:"st.Electronics.electronics15", backgroundColor:"#ffffff"
+      }
+
+      standardTile("hdmi1", "device.switch", decoration: "flat", canChangeIcon: false) {
+          state "default", label:'Input: HDMI1', action:"hdmi1", icon:"st.Electronics.electronics5", backgroundColor:"#ffffff"
+      }
+
+      main "switch"
+      details([ "power", "mute", "tv", "hdmi1"])
+  }
 }
 
 def parse(String description) {
     log.debug "Parsing '${description}'"
+    
+    def msg = parseLanMessage(description)
+    log.debug "parsed message: $msg"
+
+    def headersAsString = msg.header // => headers as a string
+    def headerMap = msg.headers      // => headers as a Map
+    def body = msg.body              // => request body as a string
+    def status = msg.status          // => http status code of the response
+    def json = msg.json              // => any JSON included in response body, as a data structure of lists and maps
+    def xml = msg.xml                // => any XML included in response body, as a document tree structure
+    def data = msg.data  
+
+    log.debug "headers: $headerMap"
+    log.debug "status: $status"
+    log.debug "data: $data"
+    log.debug "xml: $xml"
+    log.debug "json: $json"
+
+    if (msg.xml) {
+      log.debug "todo: check SOAP contents"
+      //def rootNode = new XmlSlurper().parseText(msg.xml)
+      //log.debug rootNode
+
+      def dummyOn = createEvent(name: "switch", value: "on")
+      return [dummyOn]      
+    }
+
+    //def dummyOn = createEvent(name: "switch", value: "on")
+    //return [dummyOn]
+
     return null;
 }
 
 def off() {
 	log.debug "Turning TV OFF"  
-  	sendCommand('command', 'X_SendKey', 'POWER')
-//  sendEvent(name:"Command", value: "Power Off", displayed: true) 
+  
+  sendEvent(name:"Command", value: "Power Off", displayed: true)
+  return sendCommand('POWER')
 }
 
 def mute() {
 	log.debug "Toggle Mute"  
-	sendCommand('command', 'X_SendKey', 'MUTE')
-    //sendEvent(name:"Command", value: "Mute", displayed: true) 
+	sendEvent(name:"Command", value: "Mute", displayed: true) 
+  return sendCommand('MUTE')
 }
 
-// handle commands
-def sendCommand(type, action, commandCode) {
+def tv() {
+	log.debug "Switch input: TV"  
+	sendEvent(name:"Command", value: "Switch input: TV", displayed: true) 
+  return sendCommand('TV')
+}
+
+def hdmi1() {
+	log.debug "Switch input: $Hdmi1Label (HDMI1)"  
+	sendEvent(name:"Command", value: "Switch input: HDMI1", displayed: true) 
+  return sendCommand('HDMI1')
+}
+
+def checkVolume() {
+	log.debug "Checking volume"  
+  return sendQuery()
+}
+
+def sendQuery() {
+  return sendRawRequest('render', 'GetVolume', '<InstanceID>0</InstanceID><Channel>Master</Channel>')
+}
+
+def sendCommand(commandCode) {
+  return sendRawRequest('command', 'X_SendKey', "<X_KeyEvent>NRC_$commandCode-ONOFF</X_KeyEvent>")
+}
+
+def sendRawRequest(type, action, command) {
     def host = TvIP 
     def port = 55000
-    def command = "<X_KeyEvent>NRC_$commandCode-ONOFF</X_KeyEvent>"
     def hosthex = convertIPtoHex(host).toUpperCase() //thanks to @foxxyben for catching this
     def porthex = convertPortToHex(port).toUpperCase()
     device.deviceNetworkId = "$hosthex:$porthex" 
@@ -93,9 +152,9 @@ def sendCommand(type, action, commandCode) {
 
     log.debug "ULR: $url, URN: $urn"
   
-    def headers = ["Content-Type": "text/xml; charset=\"utf-8\""] 
-    headers.put("HOST", "$host:$port")
-    headers.put("SOAPACTION", "\"urn:$urn#$action\"")
+    def headers = ["Content-Type": "text/xml; charset=\"utf-8\"",
+          "HOST": "$host:$port",
+          "SOAPACTION": "\"urn:$urn#$action\""]
     
     log.debug "The Header is $headers"
 
@@ -118,9 +177,10 @@ def sendCommand(type, action, commandCode) {
     	headers: headers
     )
         	
-    hubAction.options = [outputMsgToS3:true]
+    //hubAction.options = [outputMsgToS3:true]
     log.debug hubAction
-    hubAction
+
+    return hubAction
     }
     catch (Exception e) {
     	log.debug "Hit Exception $e on $hubAction"
